423a3f2446c21e42f859a91cb0ddc834f3c2aea2
Himanshunagar07 <himanshunagar5507@gmail.com>
2026-06-07 17:44:04 +0530

    Add files via upload

---
+++ StringLiteralEX.java
@@ -0,0 +1,12 @@
+public class StringLiteralEX {
+    public static void main(String []args){
+        String s="Himanshu Nagar";
+        //  String s='jdkasjd';//error
+       // String s= dasskj;//error
+        String ss="123";
+        String sss="33.56";
+        System.out.println(s);
+        System.out.println(ss);
+        System.out.println(sss);}
+    
+}
