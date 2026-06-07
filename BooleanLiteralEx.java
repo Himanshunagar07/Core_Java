ae722691acfd2a79be8fb985b81b0e2fa45bb034
Himanshunagar07 <himanshunagar5507@gmail.com>
2026-06-07 18:39:52 +0530

    Add BooleanLiteralEx class to demonstrate boolean literals

---
+++ BooleanLiteralEx.java
@@ -0,0 +1,12 @@
+public class BooleanLiteralEx {
+    public static void main(String[] args){
+        boolean b1=true;
+        boolean b2=false;
+        // boolean a=True; //Not Allowed
+        // boolean b=False; //Not Allowed
+        // boolean a='true'; //Not Allowed
+        // boolean b="false"; //Not Allowed
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
