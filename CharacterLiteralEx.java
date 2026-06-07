dea81d7f96c73281f71a8255dd2957db59cd0323
Himanshunagar07 <himanshunagar5507@gmail.com>
2026-06-07 18:15:33 +0530

    Add CharacterLiteralEx class to illustrate char usage
    Demonstrate character literals and ASCII/Unicode in Java.

---
+++ CharacterLiteralEx.java
@@ -0,0 +1,18 @@
+public class CharacterLiteralEx {
+    public static void main(String[] args){
+        char c='h';//Single quote and single digit
+       // char C="s";
+        //char c='sdaasd';//Error
+        char c1=104;//ACSII code 0-255
+        char c2='\u0068'; // Uni code 0-65535
+        System.out.println(c);      
+        System.out.println(c1);
+        System.out.println(c2);
+        //ASCII CODE(0-255)UNI CODE(0-65535)
+        //A_Z=65-90
+        //a-z=97-122
+        //0-9=48-57 
+        //space=32
+        //Enter=10
+    }
+}
